d1612945fada952924363d80c9fb86e128682538
João Manfré <126287501+joaomanfre@users.noreply.github.com>
2023-06-30 15:24:01 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-function frutas() {
-    let fruta = "fruta";
-    fruta = prompt("Nome da fruta")
-    switch (fruta) {
-        case "Maçã":
-            alert("Não vendemos essa fruta aqui!");
-            break;
-        case "Kiwi":
-            alert("Estamos com escassez de Kiwis!");
-            break;
-        case "Melancia":
-            alert("Aqui está, são R$3,00 KG!");
-            break;
-        default:
-            alert("Erro no console");
-    }
+let fruta = 'fruta'
+
+let span = document.getElementById('texto')
+fruta = prompt('O que deseja?')
+
+switch (fruta.toLowerCase()) {
+    case 'maça':
+        alert('Não vendemos essa fruta aqui')
+        break
+    case 'kiwi':
+        alert('Estamos com escassez de kiwis')
+        break
+    case 'melancia':
+        alert('Aqui está, são 3 reais o quilo')
+        break
+    default:
+        alert("Recarregue a página e experimento novos resultados")
 }
